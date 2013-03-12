a56f20516cc01d7194d2b3a1946737d74026a005
datianshi <dsz0111@gmail.com>
2013-03-12 13:11:48 -0500

    Init the repo

---
+++ poll/src/main/java/org/freelance/message/poll/App.java
@@ -0,0 +1,13 @@
+package org.freelance.message.poll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
